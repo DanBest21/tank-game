d910bf92b268318e9163549c62b2873a47e9bd5c
lebedys <adomas.lebedys@protonmail.com>
2021-02-14 14:46:36 +0000

    clean up comments, add todos, add stylesheet

--- src/index.js
+++ src/index.js
@@ -2,42 +2,13 @@ import { startCapturingInput, stopCapturingInput } from './input.js';
 import { startRendering, stopRendering } from './render.js';
 import { startEventLoop } from './input.js'
 
-
-// Learn more about this file at:
-// https://victorzhou.com/blog/build-an-io-game-part-1/#3-client-entrypoints
-// import { connect, play } from './networking';
-// import { downloadAssets } from './assets';
-// import { initState } from './state';
-// import { setLeaderboardHidden } from './leaderboard';
-
-// I'm using a tiny subset of Bootstrap here for convenience - there's some wasted CSS,
-// but not much. In general, you should be careful using Bootstrap because it makes it
-// easy to unnecessarily bloat your site.
-// import './css/bootstrap-reboot.css';
-// import './css/main.css';
-
-// window.onload()
-
 // const playMenu = document.getElementById('play-menu');
 const playButton = document.getElementById('play-button');
 // const usernameInput = document.getElementById('username-input');
 
 playButton.onclick = () => {
-  // play(usernameInput.value);
-  // playMenu.classList.add('hidden');
-  // initState();
-  // console.log("alksjd");
+  // todo - initialize state somehow 
   startCapturingInput();
-  startEventLoop();
-  startRendering();
-  // setLeaderboardHidden(false);
+  startEventLoop(); // state updates
+  startRendering(); // canvas updates
 };
-
-
-
-// function onGameOver() {
-//   stopCapturingInput();
-//   stopRendering();
-//   playMenu.classList.remove('hidden');
-//   setLeaderboardHidden(true);
-// }
--- src/input.js
+++ src/input.js
@@ -1,7 +1,3 @@
-// Learn more about this file at:
-// https://victorzhou.com/blog/build-an-io-game-part-1/#6-client-input-%EF%B8%8F
-// import { updateDirection } from './networking';
-
 // TODO: Delete later.
 const MAP_SIZE = 500;
 const SPEED = 3;
@@ -9,30 +5,27 @@ const ROTATION_SPEED = 3;
 var me = {"x": MAP_SIZE / 2, "y": MAP_SIZE / 2, "theta": 0, "speed": 0, "angular_velocity": 0};
 
 // todo - should theta be renamed? or otherwise, should speed be renamed to 'r' to be in mathematical notation?
+// todo - possibly add lines showing coordinates on screen of every object (player) for debugging purposes
 
 function onKeyDown(e) {
   switch (e.keyCode) {
       case 37:
       case 65: 
-        // rotatePlayer(ROTATION_SPEED);
         me.angular_velocity = ROTATION_SPEED;
         console.log("Left pressed"); 
         break; //Left key
       case 38:
       case 87: 
-        // movePlayer(SPEED);
         me.speed = SPEED;
         console.log("Up pressed"); 
         break; //Up key
       case 39:
       case 68: 
-        // rotatePlayer(-ROTATION_SPEED);
         me.angular_velocity = -1*ROTATION_SPEED;
         console.log("Right pressed"); 
         break; //Right key
       case 40:
       case 83:
-        // movePlayer(-SPEED); 
         me.speed = -SPEED;
         console.log("Down pressed"); 
         break; //Down key
@@ -44,25 +37,21 @@ function onKeyUp(e) {
   switch (e.keyCode) {
     case 37:
     case 65: 
-      // rotatePlayer(ROTATION_SPEED);
       me.angular_velocity = 0;
       console.log("Left released"); 
       break; //Left key
     case 38:
     case 87: 
-      // movePlayer(SPEED);
       me.speed = 0;
       console.log("Up released"); 
       break; //Up key
     case 39:
     case 68: 
-      // rotatePlayer(-ROTATION_SPEED);
       me.angular_velocity = 0;
       console.log("Right released"); 
       break; //Right key
     case 40:
     case 83:
-      // movePlayer(-SPEED); 
       me.speed = 0;
       console.log("Down released"); 
       break; //Down key
@@ -81,10 +70,11 @@ function movePlayer(player) {
 }
 
 function update() {
-  // todo - update position by d_pos=speed, angle by d_angle=angular_velocity
-
   rotatePlayer(me);
   movePlayer(me);
+
+  // todo - update all bullets
+  // todo - update all others
 }
 
 export function getMe() {
@@ -101,10 +91,8 @@ export function stopCapturingInput() {
   window.removeEventListener('keyup', onKeyUp);
 }
 
-// let updateInterval = setInterval(update, 1000 / 60);
 let updateInterval;
-
-export function startEventLoop() { // todo - rename function?
+export function startEventLoop() {
   clearInterval(updateInterval);
   updateInterval = setInterval(update, 1000 / 60);
 }
